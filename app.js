0101c51cc91a51c8016b33e8bc66f58ecc150843
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2020-12-06 22:16:10 +0530

    added input and output

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello, world!")
+var btnTranslate = document.querySelector("#btn-translate");
+var inputTxt = document.querySelector("#inputTxt");
+var outputTxt = document.querySelector("#outputTxt");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json/"
+
+function clickHandler() {
+    console.log("Clicked");
+    outputTxt.innerText = "asjklg " + inputTxt.value;
+};
+
+btnTranslate.addEventListener("click", clickHandler);
